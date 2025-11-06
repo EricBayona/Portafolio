31d2f1ffce4a1569e1b3500c1503ac506fe763ed
Eric Bayona <ericjf.bayona@gmail.com>
2025-11-06 18:21:39 -0300

    correccion de margenes seccion

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import FadeInSection from "./FadeInSection";
 
 export default function About() {
   return (
-    <section id="about" className="py-16 bg-white px-8">
+    <section id="about" className="py-25 bg-white px-8">
       <FadeInSection>
         <h3 className="text-3xl font-bold text-center text-blue-800 mb-6">
           Sobre mí
